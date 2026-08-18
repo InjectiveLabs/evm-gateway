13d53cdd31e281b7f1f1700b14e3c33f391ab708
Dusan Brajovic <dbrajovic3@gmail.com>
2026-08-18 13:59:34 +0200

    chore: use ibc address hex from evm types

--- internal/evm/rpc/virtual/ibc/ibc.go
+++ internal/evm/rpc/virtual/ibc/ibc.go
@@ -21,7 +21,7 @@ const EventType = "injective.evm.v1.EventIBCHookCall"
 
 var (
 	// ContractAddress is the trusted EVM caller used by the IBC hook.
-	ContractAddress = common.HexToAddress("0x0000000000000000000000000000000000000069")
+	ContractAddress = common.HexToAddress(evmtypes.IBCHookCallerAddressHex)
 	TopicHookCall   = crypto.Keccak256Hash([]byte("IBCHookCall(string,string,uint64,address,bool,bytes,string)"))
 
 	summaryArgs = abi.Arguments{
